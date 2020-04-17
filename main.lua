9aa44d48567fb9e72838403bb2a46d62358f3aba
hilalisadev <hilali.samir@hotmail.fr>
2020-04-17 17:43:23 +0200

    add loseball

--- main.lua
+++ main.lua
@@ -1,7 +1,16 @@
+ballx=64
+bally=64
+ballsize=3
+ballxdir = 5
+ballydir = 3
+
 padx =53
 pady=122
 padw =24
 padh=4
+score =0
+lives=3
+
 
 function movepaddle() 
     if btn(0) then
@@ -17,6 +26,11 @@ end
 
 function _draw()
     rectfill(0,0,128,128,3)
+    print(score, 12,6,15)
+    for i= 1, lives do
+        spr(001,100,4)
+    end
+    print(lives, 110,6,15)
     rectfill(padx, pady, padx+padw, pady+padh, 15)
     circfill(ballx, bally, ballsize, 15)
 end
@@ -42,6 +56,7 @@ function _update()
     movepaddle()
     moveball()
     bounceball()
+    loseball()
 end
 
 function bounceball()
@@ -64,5 +79,24 @@ function bouncepaddle()
     if ballx >= padx and ballx <= padx + padw and bally > pady then
         sfx(6)
         ballydir =- ballydir
+        score +=10
     end
 end
+
+function loseball()
+    if bally > 128 then
+        if lives > 0 then 
+            sfx(3)
+            bally= 24
+            lives -=1
+        end
+    else if lives == 0 then
+        ballydir = 0
+        ballxdir = 0
+        bally = 64
+        sfx(5)
+    end
+end
+end
+
+
